345da4f4f3e0679c371568f9bdf629407c874d4d
Lukman Aditiya <82071061+Lukman-bot@users.noreply.github.com>
2023-03-06 13:52:42 +0700

    Update Database > 06/03/2023

---
+++ db.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 06, 2023 at 07:52 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lukman_tutorial_website_belajar_laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id_pengguna` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` text NOT NULL,
+  `nama_lengkap` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinsi`
+--
+
+CREATE TABLE `provinsi` (
+  `id_provinsi` char(2) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `provinsi`
+--
+
+INSERT INTO `provinsi` (`id_provinsi`, `nama`) VALUES
+('11', 'ACEH'),
+('12', 'SUMATERA UTARA'),
+('13', 'SUMATERA BARAT'),
+('14', 'RIAU'),
+('15', 'JAMBI'),
+('16', 'SUMATERA SELATAN'),
+('17', 'BENGKULU'),
+('18', 'LAMPUNG'),
+('19', 'KEPULAUAN BANGKA BELITUNG'),
+('21', 'KEPULAUAN RIAU'),
+('31', 'DKI JAKARTA'),
+('32', 'JAWA BARAT'),
+('33', 'JAWA TENGAH'),
+('34', 'DI YOGYAKARTA'),
+('35', 'JAWA TIMUR'),
+('36', 'BANTEN'),
+('51', 'BALI'),
+('52', 'NUSA TENGGARA BARAT'),
+('53', 'NUSA TENGGARA TIMUR'),
+('61', 'KALIMANTAN BARAT'),
+('62', 'KALIMANTAN TENGAH'),
+('63', 'KALIMANTAN SELATAN'),
+('64', 'KALIMANTAN TIMUR'),
+('65', 'KALIMANTAN UTARA'),
+('71', 'SULAWESI UTARA'),
+('72', 'SULAWESI TENGAH'),
+('73', 'SULAWESI SELATAN'),
+('74', 'SULAWESI TENGGARA'),
+('75', 'GORONTALO'),
+('76', 'SULAWESI BARAT'),
+('81', 'MALUKU'),
+('82', 'MALUKU UTARA'),
+('91', 'PAPUA BARAT'),
+('94', 'PAPUA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id_pengguna`);
+
+--
+-- Indexes for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  ADD PRIMARY KEY (`id_provinsi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
