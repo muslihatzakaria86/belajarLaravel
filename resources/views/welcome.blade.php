b7a636d7fed1a16eccf02a3f6c0aa3a5d9a77750
muslihatzakaria86 <124653388+muslihatzakaria86@users.noreply.github.com>
2023-07-29 14:16:24 +0700

    Update welcome.blade.php
    tes123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,8 +55,9 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel this oke, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Laravel this is  oke, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
+                                Laravel this is  oke, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                             </div>
                         </div>
 
